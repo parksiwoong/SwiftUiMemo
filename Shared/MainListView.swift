66574ac4d0ed3a0e4d5bdf91664d370597ea6f47
parksiwoong <tldnd5656@naver.com>
2022-07-20 17:10:50 +0900

    insert작업중

--- Shared/MainListView.swift
+++ Shared/MainListView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct MainListView: View {
+    @EnvironmentObject var store: MemoStore //뷰가 생성되는 시점에 공유 데이터를 확인하고 스토어속성과 동일한 타입이 등록돼 있다면 속성을 자동으로 초기화 해줌
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(store.list){ memo in
+                VStack(alignment: .leading) {
+                    Text(memo.content)
+                        .font(.body)
+                        .lineLimit(1)
+                    Text(memo.insertDate, style: .date)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("내 메모")
+        }
+        
     }
 }
 
 struct MainListView_Previews: PreviewProvider {
     static var previews: some View {
         MainListView()
+            .environmentObject(MemoStore())
     }
 }
--- Shared/SwiftUiMemoApp.swift
+++ Shared/SwiftUiMemoApp.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 @main
 struct SwiftUiMemoApp: App {
+    @StateObject var store = MemoStore() // 메모저장소에서 모두 사용할 수 있도록
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store)
         }
     }
 }
