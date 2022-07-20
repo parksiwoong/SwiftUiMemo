b468b7fb1e9fb81896393d2ddd1ed1291ba65af6
parksiwoong <tldnd5656@naver.com>
2022-07-20 15:19:23 +0900

    Start

---
+++ Shared/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  SwiftUiMemo
+//
+//  Created by si woong park on 2022/07/20.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
